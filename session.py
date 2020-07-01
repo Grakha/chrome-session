d66be4fa06d75cd73c67d912ed410b48b2156c77
Igor <26maius@gmail.com>
2020-07-02 02:27:15 +0400

    methods for selecting dirs and btns

--- session.py
+++ session.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import tkinter as tk
 from tkinter import filedialog
+import os
 
 # Main window
 window = tk.Tk()
@@ -15,34 +16,77 @@ window.geometry("370x340")
 
 
 # Functions for Choose Paths
-def base_path():
-    tk.Label(window, text="Base Path").pack()
-    file_path = filedialog.askdirectory()
-
-def output_path():
-    tk.Label(window, text="Output Path").pack()
-    file_path = filedialog.askdirectory()
-
+def close_window():
+    window.destroy()
 
 # Body
 title = tk.Label(window, text="Google Save Session", anchor='w').pack(fill='both')
 
+photo = tk.PhotoImage(file="./icons/folder.png")
+
+
+
+# def initial_dir():
+#     dir_path = filedialog.askdirectory(initialdir=entry_path_in, title="Select Folder Where Files is")
+#     if dir_path:
+#         entry_in.delete('0', 'end')
+#         entry_in.insert(0, dir_path)
+        
+    # entry_path_in = dir_path
+
+# def output_dir():
+#     dir_path = filedialog.askdirectory(initialdir=entry_path_out, title="Select Folder Where Save Files")
+#     if dir_path:
+#         entry_out.delete('0', 'end')
+#         entry_out.insert(0, dir_path)
+    # entry_path_out = dir_path
+
+class SelectDir:
+
+    def __init__(self, dir_in, dir_out):
+        self.dir_in = dir_in
+        self.dir_out = dir_out
+        self.label_in = "Select Folder Where Files is"
+        self.label_out = "Select Folder Where Save Files"
+
+    def initial_dir(self):
+        path_name = filedialog.askdirectory(initialdir=self.dir_in, title=self.label_in)
+        print(path_name)
+        if path_name:
+            entry_in.delete('0', 'end')
+            entry_in.insert(0, path_name)
+            self.dir_in = path_name
+
+    def output_dir(self):
+        path_name = filedialog.askdirectory(initialdir=self.dir_out, title=self.label_out)
+        if path_name:
+            entry_out.delete('0', 'end')
+            entry_out.insert(0, path_name)
+            self.dir_out = path_name
+
+
 # Path in
 label_in = tk.Label(window, text="Input PATH").pack()
-entry_in = tk.Entry(window, width="35").pack()
-
-photo = tk.PhotoImage(file="./icons/folder.png")
-btn_entry_in = tk.Button(window, image=photo, width="16", height="16", command=base_path).pack()
+entry_in = tk.Entry(window, width="35")
+entry_path_in = entry_in.insert(0, 'C:/')
+entry_in.pack()
 
 # Path out
 label_out = tk.Label(window, text="Output PATH").pack()
-entry_out = tk.Entry(window, width="35").pack()
+entry_out = tk.Entry(window, width="35")
+entry_path_out = entry_out.insert(0, 'D:/')
+entry_out.pack()
 
-btn_entry_out = tk.Button(window, image=photo, width="16", height="16", command=output_path).pack()
+btn_init = SelectDir(entry_in.get(), entry_out.get())
+
+tk.Button(window, image=photo, width="16", height="16", command=btn_init.initial_dir).pack()
+
+tk.Button(window, image=photo, width="16", height="16", command=btn_init.output_dir).pack()
+
 
 
 # buttons
-btn_cancel = tk.Button(window, text="Cancel").pack()
+btn_cancel = tk.Button(window, text="Cancel", command=close_window).pack()
 
 btn_ch_path = tk.Button(window, text="Change PATH").pack()
 
