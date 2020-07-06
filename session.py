a987ef7d9d9fd6626965f31193fa508c8efd9be4
Igor <26maius@gmail.com>
2020-07-06 19:21:46 +0400

    methods copy files, dirs src and dst

--- session.py
+++ session.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import os
+import shutil
 
 # Main window
 window = tk.Tk()
@@ -13,86 +14,78 @@ window.title('Files Save')
 window.geometry("370x340")
 
 # window.withdraw()
-
-
-# Functions for Choose Paths
-def close_window():
-    window.destroy()
-
 # Body
-title = tk.Label(window, text="Google Save Session", anchor='w').pack(fill='both')
+tk.Label(window, text="Google Save Session", anchor='w').pack(fill='both')
 
 photo = tk.PhotoImage(file="./icons/folder.png")
 
-
-
-# def initial_dir():
-#     dir_path = filedialog.askdirectory(initialdir=entry_path_in, title="Select Folder Where Files is")
-#     if dir_path:
-#         entry_in.delete('0', 'end')
-#         entry_in.insert(0, dir_path)
-        
-    # entry_path_in = dir_path
-
-# def output_dir():
-#     dir_path = filedialog.askdirectory(initialdir=entry_path_out, title="Select Folder Where Save Files")
-#     if dir_path:
-#         entry_out.delete('0', 'end')
-#         entry_out.insert(0, dir_path)
-    # entry_path_out = dir_path
-
 class SelectDir:
 
-    def __init__(self, dir_in, dir_out):
-        self.dir_in = dir_in
-        self.dir_out = dir_out
+    def __init__(self, src, dst):
+        self.gui()
+        self.src = src
+        self.dst = dst
         self.label_in = "Select Folder Where Files is"
         self.label_out = "Select Folder Where Save Files"
 
-    def initial_dir(self):
-        path_name = filedialog.askdirectory(initialdir=self.dir_in, title=self.label_in)
-        print(path_name)
-        if path_name:
+    def gui(self):
+        pass
+
+    def browse_folder(self):
+        src = filedialog.askdirectory(initialdir=self.src, title=self.label_in)
+        if src:
             entry_in.delete('0', 'end')
-            entry_in.insert(0, path_name)
-            self.dir_in = path_name
+            entry_in.insert(0, src)
+            self.src = src
 
-    def output_dir(self):
-        path_name = filedialog.askdirectory(initialdir=self.dir_out, title=self.label_out)
-        if path_name:
+    def move_to(self):
+        dst = filedialog.askdirectory(initialdir=self.dst, title=self.label_out)
+        if dst:
             entry_out.delete('0', 'end')
-            entry_out.insert(0, path_name)
-            self.dir_out = path_name
+            entry_out.insert(0, dst)
+            self.dst = dst
+
+    def copy_file(self):
+        for item in os.listdir(self.src):
+            if item.endswith('Session') or item.endswith('Tabs'):
+                file_name = os.path.join(os.path.normpath(self.src), item)
+                print(file_name)
+                shutil.copy(file_name, self.dst)
+
+    def clear_in(self):
+        self.src = entry_in.delete('0', 'end')
+
+    def clear_out(self):
+        self.dst = entry_out.delete('0', 'end')
+
+    def close_window(self):
+        window.destroy()
 
 
-# Path in
-label_in = tk.Label(window, text="Input PATH").pack()
+
+# Input src
+tk.Label(window, text="Input PATH").pack()
 entry_in = tk.Entry(window, width="35")
-entry_path_in = entry_in.insert(0, 'C:/')
+entry_in.insert(0, 'C:/')
 entry_in.pack()
 
-# Path out
-label_out = tk.Label(window, text="Output PATH").pack()
+# Input dst
+tk.Label(window, text="Output PATH").pack()
 entry_out = tk.Entry(window, width="35")
-entry_path_out = entry_out.insert(0, 'D:/')
+entry_out.insert(0, 'D:/')
 entry_out.pack()
 
 btn_init = SelectDir(entry_in.get(), entry_out.get())
 
-tk.Button(window, image=photo, width="16", height="16", command=btn_init.initial_dir).pack()
-
-tk.Button(window, image=photo, width="16", height="16", command=btn_init.output_dir).pack()
-
-
-
 # buttons
-btn_cancel = tk.Button(window, text="Cancel", command=close_window).pack()
+tk.Button(window, image=photo, width="16", height="16", command=btn_init.browse_folder).pack()
+tk.Button(window, text="X", command=btn_init.clear_in).pack()
 
-btn_ch_path = tk.Button(window, text="Change PATH").pack()
+tk.Button(window, image=photo, width="16", height="16", command=btn_init.move_to).pack()
+tk.Button(window, text="X", command=btn_init.clear_out).pack()
 
-btn_save = tk.Button(window, text="Save").pack()
-
-btn_cp = tk.Button(window, text="Copy").pack()
+tk.Button(window, text="Cancel", command=btn_init.close_window).pack()
+tk.Button(window, text="Copy", command=btn_init.copy_file).pack()
 
 # Tkinter event loop
 window.mainloop()
